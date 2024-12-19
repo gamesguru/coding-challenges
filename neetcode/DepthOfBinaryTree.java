2ff55fe60478722e81cdde4c47beea405c71a9d2
Anton Sakhanovych <antonsakhanovych@gmail.com>
2024-12-18 20:09:47 -0500

    depth of binary tree

---
+++ neetcode/DepthOfBinaryTree.java
@@ -0,0 +1,29 @@
+package neetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class DepthOfBinaryTree {
+    public static int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(10);
+        TreeNode b = new TreeNode(5, new TreeNode(7), new TreeNode(8));
+        TreeNode root = new TreeNode(1, a, b);
+        System.out.println("Should be 3: " + DepthOfBinaryTree.maxDepth(root));
+    }
+}
